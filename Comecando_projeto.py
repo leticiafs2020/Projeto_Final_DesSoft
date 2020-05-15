d4f4eeb1d29a96ea8dbb37d354a0f44c1c908f66
nataliaqmc <nataliaqmc@al.insper.edu.br>
2020-05-15 07:30:48 -0300

    imagens e bibliotecas

--- Comecando_projeto.py
+++ Comecando_projeto.py
@@ -1,1 +1,6 @@
-
+#Importando as bibliotecas necessárias:
+import pygame
+import random
+from config import WIDTH, HEIGHT, INIT, GAME, QUIT
+from init_screen import init_screen
+from game_screen import game_screen
