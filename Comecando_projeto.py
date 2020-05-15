ce237026c8554a9a8f74d18a96dec08404679a20
nataliaqmc <nataliaqmc@al.insper.edu.br>
2020-05-15 07:36:33 -0300

    s

--- Comecando_projeto.py
+++ Comecando_projeto.py
@@ -1,6 +1,3 @@
 #Importando as bibliotecas necessárias:
 import pygame
 import random
-from config import WIDTH, HEIGHT, INIT, GAME, QUIT
-from init_screen import init_screen
-from game_screen import game_screen
