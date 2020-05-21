ea4343fdeb222194a3a23eb9c4f3245e7a210834
AmandaColucci <62797435+AmandaColucci@users.noreply.github.com>
2020-05-21 17:18:29 -0300

    Adicionei dentro da classe player a imagem

--- Comecando_projeto.py
+++ Comecando_projeto.py
@@ -51,8 +51,8 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, game):
         pygame.sprite.Sprite.__init__(self)
         self.game= game
-        self.image= pygame.Surface((30,40))
-        self.image.fill(amarelo)
+        self.image= self.game.spritesheet.get_image(260, 1032, 128, 256)
+        self.image.set_colorkey(preto)
         self.rect= self.image.get_rect()
         self.rect.center= (WIDTH / 2, HEIGHT / 2)
         self.pos= vet(WIDTH / 2, HEIGHT / 2) #posição
