ce73b6af4cd98ea514ce5c014072173bbc435837
AmandaColucci <62797435+AmandaColucci@users.noreply.github.com>
2020-05-21 11:03:45 -0300

    defini as cores do jogo

--- Comecando_projeto.py
+++ Comecando_projeto.py
@@ -1,3 +1,17 @@
 #Importando as bibliotecas necessárias:
 import pygame
 import random
+from settings import *
+from sprites import *
+from os import path
+ 
+# definindo as cores
+branco= (255, 255, 255)
+preto= (0, 0, 0)
+vermelho= (255, 0, 0)
+verde= (0, 255, 0)
+azul= (0, 0, 255)
+amarelo= (255, 255, 0)
+azul_claro= (0, 155, 155)
+fundo= azul_claro
+
