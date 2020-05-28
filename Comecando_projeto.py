d2fadb82210a1e75136e3b53e034553241dc7b53
leticiafs2020 <62616473+leticiafs2020@users.noreply.github.com>
2020-05-28 18:39:46 -0300

    Propriedades do jogo

--- Comecando_projeto.py
+++ Comecando_projeto.py
@@ -33,6 +33,10 @@ l_plataformas= [(0, HEIGHT-40, WIDTH, 40), (175, 100, 50, 20),
 (WIDTH / 2 - 50, HEIGHT * 3 /4, 100, 20),
 (350, 200, 100, 20), (125, HEIGHT - 350, 100, 20)] 
 
+#propriedades do jogo
+boost_poder= 60
+spawn_pct_po= 7 #tempo p/ responder 
+
 vet= pygame.math.Vector2 # é usado o vetor para o movimento 
 
 class Spritesheet:
