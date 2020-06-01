30713683078de1ddaa19a1e3b3afae223e40dddf
nataliaqmc <nataliaqmc@al.insper.edu.br>
2020-06-01 17:37:49 -0300

    fazendo os imports nos diferentes files

---
+++ Classes.py
@@ -0,0 +1,6 @@
+import Configuracoes
+import Iniciando
+import Jogo
+from random import randrange, choice
+from os import path
+import pygame
---
+++ Jogo.py
@@ -0,0 +1,6 @@
+import Classes
+import Iniciando
+import Configuracoes
+from random import randrange, choice
+from os import path
+import pygame
